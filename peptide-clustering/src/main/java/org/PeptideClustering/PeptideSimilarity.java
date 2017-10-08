0e532738df964827a0c2fe4d0fbbe5005eb76b67
Evgeny Zemskov <evgeny.zemskov@kronshtadt.ru>
2017-10-08 22:10:21 +1100

    Added peptide-clustering project stub

---
+++ peptide-clustering/src/main/java/org/PeptideClustering/PeptideSimilarity.java
@@ -0,0 +1,11 @@
+package org.PeptideClustering;
+
+import info.debatty.spark.kmedoids.Similarity;
+
+public class PeptideSimilarity implements Similarity<String> 
+{
+    public double similarity(String p1, String p2)
+    {
+        return 0;
+    }
+}
