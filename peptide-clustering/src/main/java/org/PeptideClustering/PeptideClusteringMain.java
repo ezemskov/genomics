5e6fa2e9952fd8923b727bd60652a7ce4004e553
Evgeny Zemskov <evgeny.zemskov@kronshtadt.ru>
2017-10-10 19:54:35 +1100

    Fill and print SolutionClusters (addded to spark-kmedoids)

--- peptide-clustering/src/main/java/org/PeptideClustering/PeptideClusteringMain.java
+++ peptide-clustering/src/main/java/org/PeptideClustering/PeptideClusteringMain.java
@@ -2,11 +2,12 @@ package org.PeptideClustering;
 
 import info.debatty.spark.kmedoids.Clusterer;
 import info.debatty.spark.kmedoids.Solution;
+import info.debatty.spark.kmedoids.SolutionClusters;
+import info.debatty.spark.kmedoids.Cluster;
 import info.debatty.spark.kmedoids.budget.TrialsBudget;
 import info.debatty.spark.kmedoids.neighborgenerator.ClaransNeighborGenerator;
 import java.io.Serializable;
-//import org.apache.log4j.Level;
-//#import org.apache.log4j.Logger;
+import java.util.Iterator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -31,30 +32,36 @@ public class PeptideClusteringMain
 {
     public static void main(String[] args) throws Exception 
     {
-        //try
+        SparkConf conf = new SparkConf()
+                .setAppName("Spark k-medoids clusterer");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        SQLContext sqlc = new SQLContext(jsc);
+
+        PeptideGenFunc gen = new PeptideGenFunc();
+        JavaRDD<String> pepts = sqlc.range(0, 100, 1, 2)
+                .map(gen, Encoders.STRING())
+                .toJavaRDD();
+
+        Clusterer<String> clusterer = new Clusterer<>();
+        clusterer.setK(5);
+        clusterer.setSimilarity(new PeptideSimilarity());
+        clusterer.setNeighborGenerator(new ClaransNeighborGenerator<>());
+        clusterer.setBudget(new TrialsBudget(30));
+        Solution<String> res = clusterer.cluster(pepts);
+
+        SolutionClusters<String> res2 = new SolutionClusters<>();
+        res2.setSimilarity(new PeptideSimilarity());
+        res2.AssignElemsToClusters(pepts.collect(), res.getMedoids());
+            
+        System.out.println(res.toString());
+        for (Cluster<String> cluster : res2.getClusters())
         {
-            SparkConf conf = new SparkConf()
-                    .setAppName("Spark k-medoids clusterer");
-            JavaSparkContext jsc = new JavaSparkContext(conf);
-            SQLContext sqlc = new SQLContext(jsc);
-
-            PeptideGenFunc gen = new PeptideGenFunc();
-            JavaRDD<String> pepts = sqlc.range(0, 1000, 1, 2)
-                    .map(gen, Encoders.STRING())
-                    .toJavaRDD();
-
-            Clusterer<String> clusterer = new Clusterer<>();
-            clusterer.setK(10);
-            clusterer.setSimilarity(new PeptideSimilarity());
-            clusterer.setNeighborGenerator(new ClaransNeighborGenerator<>());
-            clusterer.setBudget(new TrialsBudget(30));
-            Solution<String> res = clusterer.cluster(pepts);
-            System.out.println(res.toString());
-            for (String medoid : res.getMedoids())
+            System.out.format("\nMedoid : %s\n", cluster.medoid);
+            for (Cluster.ElemSim<String> elem : cluster.elems)
             {
-                System.out.format("Medoid : %s\n", medoid);
+                System.out.format("\tElement : %s %f.0\n", elem.elem, elem.sim);
             }
-            jsc.close();
         }
+        jsc.close();
     }
 }
