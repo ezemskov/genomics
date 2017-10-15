286a5b2b5eb2b2e7357a0cde86282b761173b594
Evgeny Zemskov <evgeny.zemskov@kronshtadt.ru>
2017-10-15 11:31:54 +1100

    PSSMHCSpark : refactoring

---
+++ PSSMHCpan/src/main/java/org/PSSMHC/Impl.java
@@ -0,0 +1,112 @@
+package org.PSSMHC;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.sql.Row;
+
+public class Impl
+{
+    public static class ScoreFunc extends PSSMHCpan
+                    implements Serializable,
+                    Function<String,ScoredPeptide>
+    {
+        @Override
+        public ScoredPeptide call(String peptide)
+        {
+            return new ScoredPeptide(peptide, ScoreOnePeptide(peptide));
+        }
+    }
+
+    public static class BloomFunc extends PeptideBloomFilter
+                    implements Serializable,
+                    VoidFunction<ScoredPeptide>
+    {
+        @Override
+        public void call(ScoredPeptide scPep)
+        {
+            Add(scPep.peptide);
+        }
+    }
+
+    public static class GenFunc extends PeptideGen
+                    implements Serializable, 
+                    MapFunction<Row, String>
+    {
+        public String call(Row idx)
+        {
+            return Generate(idx.getLong(0));
+        }
+    }
+
+    public static class Ic50FilterFunc implements Serializable,
+                         Function<ScoredPeptide,Boolean>
+    {
+        public Ic50FilterFunc(double ic50Threshold_)
+        {
+            ic50Threshold = ic50Threshold_;
+        }
+
+        public Boolean call(ScoredPeptide scPep)
+        {
+            return (scPep.ic50 < ic50Threshold);
+        }
+
+        double ic50Threshold;
+    }
+
+    public static class CmdlineCfg
+    {
+        public long start;
+        public long end;
+        public int partitions;
+        public boolean doScore, doBinderPersist, doBinderStore, doBinderCount;
+        public int ic50Threshold;
+
+        public CmdlineCfg(String[] args, int firstArgIdx)
+        {
+            if (args.length < firstArgIdx+7)
+            {
+                throw new RuntimeException(PSSMHCpan.CmdlineHelpStr);
+            }
+
+            start =       ParseLongWithSuffix(args[firstArgIdx]);
+            end = start + ParseLongWithSuffix(args[firstArgIdx+1]);    
+            partitions =  Integer.parseUnsignedInt(args[firstArgIdx+2]);
+
+            doScore     = (Integer.parseInt(args[firstArgIdx+3]) == 1);
+            doBinderPersist   = (Integer.parseInt(args[firstArgIdx+4]) == 1);
+            doBinderStore     = (Integer.parseInt(args[firstArgIdx+5]) == 1);
+            doBinderCount     = (Integer.parseInt(args[firstArgIdx+6]) == 1);
+            if (args.length >= 8)
+            {
+                ic50Threshold = Integer.parseInt(args[firstArgIdx+7]);
+            }
+        }
+
+        private static long ParseLongWithSuffix(String val)
+        {
+            int suffixPos = val.length()-1;
+            Character suffix = val.toUpperCase().charAt(suffixPos);
+            Long multiplier = suffixes.get(suffix);
+            if ((val.length() < 2) || (multiplier == null))
+            {
+                return Long.parseUnsignedLong(val);
+            }
+
+            return multiplier * Long.parseUnsignedLong(val.substring(0, suffixPos));
+        }
+
+        private static final HashMap<Character, Long> suffixes;
+        static
+        {
+            suffixes = new HashMap<>();
+            suffixes.put('K', (long)1E3);
+            suffixes.put('M', (long)1E6);
+            suffixes.put('G', (long)1E9);
+            suffixes.put('T', (long)1E12);
+        }
+    }    
+}
--- PSSMHCpan/src/main/java/org/PSSMHC/PSSMHCSpark.java
+++ PSSMHCpan/src/main/java/org/PSSMHC/PSSMHCSpark.java
@@ -18,106 +18,6 @@ import org.apache.spark.serializer.KryoRegistrator;
 import com.esotericsoftware.kryo.Kryo; 
 import com.esotericsoftware.kryo.serializers.FieldSerializer; 
 
-
-class PSSMHCpanSparkFunc extends PSSMHCpan
-                        implements Serializable,
-                        Function<String,ScoredPeptide>
-{
-    public ScoredPeptide call(String peptide)
-    {
-        return new ScoredPeptide(peptide, ScoreOnePeptide(peptide));
-    }
-}
-
-class PeptideBloomFilterFunc extends PeptideBloomFilter
-                        implements Serializable,
-                        VoidFunction<ScoredPeptide>
-{
-    public void call(ScoredPeptide scPep)
-    {
-        Add(scPep.peptide);
-    }
-}
-
-class PeptideGenFunc extends PeptideGen
-                     implements Serializable, 
-                     MapFunction<Row, String>
-{
-    public String call(Row idx)
-    {
-        return Generate(idx.getLong(0));
-    }
-}
-
-class PeptideIc50FilterFunc implements Serializable,
-                        Function<ScoredPeptide,Boolean>
-{
-    public PeptideIc50FilterFunc(double ic50Threshold_)
-    {
-        ic50Threshold = ic50Threshold_;
-    }
-        
-    public Boolean call(ScoredPeptide scPep)
-    {
-        return (scPep.ic50 < ic50Threshold);
-    }
-
-    double ic50Threshold;
-}
-
-class PSSMHCSparkCmdlineConfig
-{
-    public long start;
-    public long end;
-    public int partitions;
-    public boolean doScore, doBinderPersist, doBinderStore, doBinderCount;
-    public int ic50Threshold;
-        
-    public PSSMHCSparkCmdlineConfig(String[] args, int firstArgIdx)
-    {
-        if (args.length < firstArgIdx+7)
-        {
-            throw new RuntimeException(PSSMHCpan.CmdlineHelpStr);
-        }
-
-        start =       ParseLongWithSuffix(args[firstArgIdx]);
-        end = start + ParseLongWithSuffix(args[firstArgIdx+1]);    
-        partitions =  Integer.parseUnsignedInt(args[firstArgIdx+2]);
-
-        doScore     = (Integer.parseInt(args[firstArgIdx+3]) == 1);
-        doBinderPersist   = (Integer.parseInt(args[firstArgIdx+4]) == 1);
-        doBinderStore     = (Integer.parseInt(args[firstArgIdx+5]) == 1);
-        doBinderCount     = (Integer.parseInt(args[firstArgIdx+6]) == 1);
-        if (args.length >= 8)
-        {
-            ic50Threshold = Integer.parseInt(args[firstArgIdx+7]);
-        }
-    }
-
-    private static long ParseLongWithSuffix(String val)
-    {
-        int suffixPos = val.length()-1;
-        Character suffix = val.toUpperCase().charAt(suffixPos);
-        Long multiplier = suffixes.get(suffix);
-        if ((val.length() < 2) || (multiplier == null))
-        {
-            return Long.parseUnsignedLong(val);
-        }
-        
-        return multiplier * Long.parseUnsignedLong(val.substring(0, suffixPos));
-    }
-
-    private static final HashMap<Character, Long> suffixes;
-    static
-    {
-        suffixes = new HashMap<>();
-        suffixes.put('K', (long)1E3);
-        suffixes.put('M', (long)1E6);
-        suffixes.put('G', (long)1E9);
-        suffixes.put('T', (long)1E12);
-    }
-}
-
 final public class PSSMHCSpark
 {
     public static class ScPepRegistrator implements KryoRegistrator 
@@ -127,8 +27,7 @@ final public class PSSMHCSpark
           kryo.register(ScoredPeptide.class, new FieldSerializer(kryo, ScoredPeptide.class)); 
         } 
     } 
-    
-    
+        
     public static void main(String[] args) throws Exception 
     {
         try
@@ -140,11 +39,11 @@ final public class PSSMHCSpark
             JavaSparkContext jsc = new JavaSparkContext(spconf);
             SQLContext sqlc = new SQLContext(jsc);
 
-            PeptideGenFunc gen = new PeptideGenFunc();
-            PSSMHCpanSparkFunc pssmhc = new PSSMHCpanSparkFunc();
+            Impl.GenFunc gen = new Impl.GenFunc();
+            Impl.ScoreFunc pssmhc = new Impl.ScoreFunc();
             int nextArgIdx = pssmhc.InitFromCmdline(args);
-            PSSMHCSparkCmdlineConfig cfg = new PSSMHCSparkCmdlineConfig(args, nextArgIdx);
-            PeptideIc50FilterFunc filterFunc = new PeptideIc50FilterFunc(cfg.ic50Threshold);
+            Impl.CmdlineCfg cfg = new Impl.CmdlineCfg(args, nextArgIdx);
+            Impl.Ic50FilterFunc filterFunc = new Impl.Ic50FilterFunc(cfg.ic50Threshold);
 
             JavaRDD<String> pepts = sqlc.range(cfg.start, cfg.end, 1, cfg.partitions)
                     .map(gen, Encoders.STRING())
