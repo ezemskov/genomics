b235f56b288e39b31f5301bee829161376a847da
Evgeny Zemskov <evgeny.zemskov@kronshtadt.ru>
2017-10-23 15:14:26 +1100

    more benchmarks

--- peptide-clustering/src/test/java/org/PeptideClustering/PeptideSimilarityBench.java
+++ peptide-clustering/src/test/java/org/PeptideClustering/PeptideSimilarityBench.java
@@ -21,80 +21,108 @@ public class PeptideSimilarityBench
     }
 
     @Test
+    public void benchmarkEmptyCall()
+    {
+        System.out.println("empty");
+        benchmark((p1, p2) -> { return 0.0; }, false);
+    }
+
+    @Test
+    public void benchmarkPosDiff1()
+    {
+        System.out.println("posDiff cast");
+        benchmark((p1, p2) -> { return (double)PeptideSimilarity.posDiff(p1, p2); }, false);
+    }
+    
+    @Test
+    public void benchmarkPosDiff2()
+    {
+        System.out.println("posDiff discard");
+        benchmark((p1, p2) -> { int res = PeptideSimilarity.posDiff(p1, p2); return 0.0; }, false);
+    }
+
+    @Test
     public void benchmarkGenOnly()
     {
         System.out.println("generate only");
-        benchmark((p1, p2) -> { return Double.NaN; } );
+        benchmark((p1, p2) -> { return Double.NaN; }, true);
     }
+
 
     @Test
     public void benchmarkSimilarity()
     {
         System.out.println("similarity");
-        benchmark((p1, p2) -> { return simCalc.similarity(p1, p2); } );
+        benchmark((p1, p2) -> { return simCalc.similarity(p1, p2); }, true);
     }
 
+/*    
     @Test
     public void benchmarkDissimilarity1()
     {
         System.out.println("dissimilarity 0.1");
-        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1/0.1); } );
+        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1/0.1); }, true);
     }
 
     @Test
     public void benchmarkDissimilarity2()
     {
         System.out.println("dissimilarity 0.2");
-        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1/0.2); } );
+        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1/0.2); }, true);
     }
 
     @Test
     public void benchmarkDissimilarity5()
     {
         System.out.println("dissimilarity 0.5");
-        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.5); } );
+        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.5); }, true);
     }
 
     @Test
     public void benchmarkDissimilarity7()
     {
         System.out.println("dissimilarity 0.7");
-        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.7); } );
+        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.7); }, true);
     }
     
     @Test
     public void benchmarkDissimilarity8()
     {
         System.out.println("dissimilarity 0.8");
-        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.8); } );
+        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.8); }, true);
     }
 
     @Test
     public void benchmarkDissimilarity9()
     {
         System.out.println("dissimilarity 0.9");
-        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.9); } );
+        benchmark((p1, p2) -> { return simCalc.dissimilarity(p1, p2, 1.0/0.9); }, true);
     }
-    
+*/    
     interface IPeptideDistance {
        public double call(String p1, String p2);
     }
     
-    private void benchmark(IPeptideDistance func)
+    private void benchmark(IPeptideDistance func, boolean doRegenerate)
     {
         Random randGen = new Random();
         PeptideGen pepGen = new PeptideGen();
         
-        final int IterationsCount = 1000000;
+        final int IterationsCount = 5000000;
         final long startTime = System.currentTimeMillis();
         int interruptedCalcCount = 0;
         
+        String p1 = pepGen.Generate(Math.abs(randGen.nextInt()));
+        String p2 = pepGen.Generate(Math.abs(randGen.nextInt()));
         for (int i=0; i<IterationsCount; ++i)
         {
-            String p1 = pepGen.Generate(Math.abs(randGen.nextInt()));
-            String p2 = pepGen.Generate(Math.abs(randGen.nextInt()));
+            if (doRegenerate)
+            {
+                p1 = pepGen.Generate(Math.abs(randGen.nextInt()));
+                p2 = pepGen.Generate(Math.abs(randGen.nextInt()));
+            }
             double res = func.call(p1, p2);
-            if (Double.isNaN(res)) {
+            if (doRegenerate && Double.isNaN(res)) {
                 ++interruptedCalcCount;
             }
         }
@@ -102,7 +130,7 @@ public class PeptideSimilarityBench
         final long durationMsec = System.currentTimeMillis() - startTime;
         final double durationAvgUsec = (double)(1E3 * durationMsec/IterationsCount);
         final double interruptedCalcFrac = interruptedCalcCount/(double)IterationsCount;
-        System.out.format("Duration total %d msec avg %.2f usec interrupted %.6f\n", 
+        System.out.format("Duration total %d msec avg %.3f usec interrupted %.6f\n", 
             durationMsec, durationAvgUsec, interruptedCalcFrac);
     }
 }
