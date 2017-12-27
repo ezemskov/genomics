61df8084453261f3b6a4f5e238044616e85a89cf
Evgeny Zemskov <evgeny.zemskov@kronshtadt.ru>
2017-12-27 14:20:34 +1100

    PSSMHC : refactoring

--- PSSMHCpan/src/main/java/org/PSSMHC/PSSMHCSpark.java
+++ PSSMHCpan/src/main/java/org/PSSMHC/PSSMHCSpark.java
@@ -12,8 +12,13 @@ import org.apache.spark.serializer.KryoRegistrator;
 import com.esotericsoftware.kryo.Kryo; 
 import com.esotericsoftware.kryo.serializers.FieldSerializer; 
 
-final public class PSSMHCSpark
+final class PSSMHCSpark
 {
+    Xml.Cfg _cfg;
+    JavaRDD<String> _pepts;
+    long _peptQnty;
+    int _peptideLength;
+    
     public static class ScPepRegistrator implements KryoRegistrator 
     { 
         @Override
@@ -23,86 +28,101 @@ final public class PSSMHCSpark
                 new FieldSerializer(kryo, Impl.ScoredPeptide.class)); 
         } 
     } 
-        
+
+    private PSSMHCSpark(Xml.Cfg cfg)
+    {
+        _cfg = cfg;
+        _pepts = null;
+        _peptQnty = -1;
+        _peptideLength = -1;
+    }
+    
+    private void processPeptideSet()
+    {
+        Impl.PSSMHCpanSparkFunc   PSSMHCpanFunc = null;
+        Impl.ScoreFilterSparkFunc filterFunc = new Impl.ScoreFilterSparkFunc(_cfg.ic50Threshold);
+        for (Xml.PSSMCfg pssmConfig : _cfg.pssmConfigs)
+        {
+            pssmConfig.peptideLength = _peptideLength;
+
+            try
+            {
+                PSSMHCpanFunc = new Impl.PSSMHCpanSparkFunc(pssmConfig);
+            }
+            catch(Exception ex)
+            {
+                System.err.println("Skipped : " + ex.getMessage());
+                continue;
+            }
+
+            JavaRDD<Impl.ScoredPeptide> binders = _pepts.map(PSSMHCpanFunc)
+                                                       .filter(filterFunc);
+            if (_cfg.doBinderPersist)
+            {
+                binders.persist(StorageLevel.MEMORY_AND_DISK_SER());
+            }
+
+            if (_cfg.doBinderStore)
+            {
+                final String resDirName = String.format("output-%s-%d", 
+                    pssmConfig.allele, pssmConfig.peptideLength);
+                binders.saveAsTextFile(resDirName, GzipCodec.class);
+            }
+
+            long binderCount = _cfg.doBinderCount ? binders.count() : -1;
+            System.out.format("%s/%d : found %d binder peptides (IC50 < %d) in total of %d\n", 
+                pssmConfig.allele, pssmConfig.peptideLength, 
+                binderCount, _cfg.ic50Threshold, _peptQnty);
+        }
+    }
+
+    public void run() throws Exception 
+    {
+        SparkConf spconf = new SparkConf()
+            .setAppName("PSSMHCSpark"); 
+
+        spconf.set("spark.kryo.registrator", ScPepRegistrator.class.getName()); 
+        JavaSparkContext jsc = new JavaSparkContext(spconf);
+        SQLContext sqlc = new SQLContext(jsc);
+
+        if (!_cfg.peptideFiles.isEmpty())
+        {
+            Xml.StringIntPair peptFile = _cfg.peptideFiles.get(0);
+            _peptideLength = peptFile.second;
+            _pepts = jsc.textFile(peptFile.first, _cfg.partitions);
+            _peptQnty = _pepts.count();
+            if (!_pepts.isEmpty() && (_pepts.first().length() != _peptideLength))
+            {
+                throw new Exception(String.format(
+                    "Invalid peptide length : %d in file, %d in xml config", 
+                    _pepts.first().length(), _peptideLength
+                ));
+            }
+        }
+        else
+        {
+            _pepts = sqlc.range(_cfg.start, _cfg.end, 1, _cfg.partitions)
+                    .map(new Impl.PeptideGenSparkFunc(), Encoders.STRING())
+                    .toJavaRDD();
+            _peptQnty = _cfg.end - _cfg.start;
+            _peptideLength = Impl.PeptideGenSparkFunc.pepLen;
+        }
+
+        if (!_cfg.doScore)   //todo : remove if
+        {
+            System.out.format("Generated %d peptides\n", _peptQnty);
+            return;
+        }
+
+        processPeptideSet();
+    }
+    
     public static void main(String[] args) throws Exception 
     {
         try
         {
-            SparkConf spconf = new SparkConf()
-                .setAppName("PSSMHCSpark"); 
-            
-            spconf.set("spark.kryo.registrator", ScPepRegistrator.class.getName()); 
-            JavaSparkContext jsc = new JavaSparkContext(spconf);
-            SQLContext sqlc = new SQLContext(jsc);
-
             Xml.Cfg cfg = new Xml.Cfg(Xml.Utils.firstOrDef(args));
-            
-            JavaRDD<String> pepts;
-            long peptQnty = -1;
-            int peptideLengthInFile = -1;
-            if (!cfg.peptideFiles.isEmpty())
-            {
-                Xml.StringIntPair peptFile = cfg.peptideFiles.get(0);
-                peptideLengthInFile = peptFile.second;
-                pepts = jsc.textFile(peptFile.first, cfg.partitions);
-                peptQnty = pepts.count();
-                if (!pepts.isEmpty() && (pepts.first().length() != peptideLengthInFile))
-                {
-                    throw new Exception(String.format(
-                        "Invalid peptide length : %d in file, %d in xml config", 
-                        pepts.first().length(), peptideLengthInFile
-                    ));
-                }
-            }
-            else
-            {
-                pepts = sqlc.range(cfg.start, cfg.end, 1, cfg.partitions)
-                        .map(new Impl.PeptideGenSparkFunc(), Encoders.STRING())
-                        .toJavaRDD();
-                peptQnty = cfg.end - cfg.start;
-            }
-                        
-            if (!cfg.doScore)
-            {
-                System.out.format("Generated %d peptides\n", peptQnty);
-                return;
-            }
-            
-            Impl.PSSMHCpanSparkFunc   PSSMHCpanFunc = null;
-            Impl.ScoreFilterSparkFunc filterFunc = new Impl.ScoreFilterSparkFunc(cfg.ic50Threshold);
-            for (Xml.PSSMCfg pssmConfig : cfg.pssmConfigs)
-            {
-                pssmConfig.peptideLength = peptideLengthInFile; //or in generator
-                
-                try
-                {
-                    PSSMHCpanFunc = new Impl.PSSMHCpanSparkFunc(pssmConfig);
-                }
-                catch(Exception ex)
-                {
-                    System.err.println("Skipped : " + ex.getMessage());
-                    continue;
-                }
-                    
-                JavaRDD<Impl.ScoredPeptide> binders = pepts.map(PSSMHCpanFunc)
-                                                           .filter(filterFunc);
-                if (cfg.doBinderPersist)
-                {
-                    binders.persist(StorageLevel.MEMORY_AND_DISK_SER());
-                }
-
-                if (cfg.doBinderStore)
-                {
-                    final String resDirName = String.format("output-%s-%d", 
-                        pssmConfig.allele, pssmConfig.peptideLength);
-                    binders.saveAsTextFile(resDirName, GzipCodec.class);
-                }
-
-                long binderCount = cfg.doBinderCount ? binders.count() : -1;
-                System.out.format("%s/%d : found %d binder peptides (IC50 < %d) in total of %d\n", 
-                    pssmConfig.allele, pssmConfig.peptideLength, 
-                    binderCount, cfg.ic50Threshold, peptQnty);
-            }
+            new PSSMHCSpark(cfg).run();
         }
         catch (Exception ex)
         {
